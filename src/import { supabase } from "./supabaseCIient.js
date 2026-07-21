6864b5e239f00911d92474d2f883a7c17ee05aee
khaledeventalhabib-eng <khaled.eventalhabib@gmail.com>
2026-07-21 16:53:29 +0300

    Update and rename src/supabaseCIient.js to src/import { supabase } from "./supabaseCIient.js

--- src/import { supabase } from "./supabaseCIient.js
+++ src/import { supabase } from "./supabaseCIient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { supabase } from "./supabaseClient";
 
 const supabaseUrl = "https://zebpzfrsmwgiswoaxxel.supabase.co";
 const supabaseAnonKey = "sb_publishable_Z0AltV4dRng56zuK4aZMfA_KrcsF9Ie";
