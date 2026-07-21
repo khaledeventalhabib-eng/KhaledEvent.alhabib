78d8506d45c1c533a9b04aba0c740dc52cf9c5c3
khaledeventalhabib-eng <khaled.eventalhabib@gmail.com>
2026-07-21 19:17:51 +0300

    Delete src/import { supabase } from "./supabaseCIient.js

--- src/import { supabase } from "./supabaseCIient.js
+++
@@ -1,6 +0,0 @@
-import { supabase } from "./supabaseClient";
-
-const supabaseUrl = "https://zebpzfrsmwgiswoaxxel.supabase.co";
-const supabaseAnonKey = "sb_publishable_Z0AltV4dRng56zuK4aZMfA_KrcsF9Ie";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
